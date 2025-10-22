77901b8b62a4929eee8cd921bd0335f42a6e70aa
Maksim Karabutov <max.karabutoff2010@yandex.ru>
2025-10-22 22:26:41 +0400

    Switched to "*" usage for spa html handler

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,32 +11,17 @@ console.log(`Running in ${env} mode`);
 app.use(cors());
 app.use(express.json());
 
-const spaHandler = (req: express.Request, res: express.Response) => {
-  res.sendFile(path.join(__dirname, "../../dist", "index.html"));
-};
-
-if (env === "production") {
-  app.use(express.static(path.join(__dirname, "../../dist")));
-
-  /*
-   * We connect directly instead of using app.get("*")
-   *The error when using "*" in Express routes occurs due to an internal conflict in the "path-to-regexp" library,
-   * which Express uses for route parsing.
-   * The * character is treated as a special route parameter. The library expects a parameter in the :param format, but receives *
-   * const regexp = pathToRegexp('*'); // A pattern like '/:foo' is expected
-   */
-  app.use((req, res, next) => {
-    if (req.method === "GET" && !req.path.startsWith("/api")) {
-      return spaHandler(req, res);
-    }
-    next();
-  });
-}
-
 // API Routes
 app.get("/api/message", (req, res) => {
   res.json({ message: "Hello, world!" });
 });
+
+if (env === "production") {
+  app.use(express.static(path.join(__dirname, "../../dist")));
+  app.get(/.*/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../../dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
